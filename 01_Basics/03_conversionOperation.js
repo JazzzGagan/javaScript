7b432e8200aa394ee761ef43ee6a40f5d5399820
Jazzz_Gagan <117917252+JazzzGagan@users.noreply.github.com>
2024-04-05 17:04:47 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "33aa";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+let isNotLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+let booleanIsNotLoggedIn = Boolean(isNotLoggedIn);
+//console.log(booleanIsNotLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "xyz" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
