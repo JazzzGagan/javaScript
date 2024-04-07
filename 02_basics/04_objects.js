935f3a8827d7c288cbcc9c32b8fce2180e4a2018
Jazzz_Gagan <117917252+JazzzGagan@users.noreply.github.com>
2024-04-07 15:27:20 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -18,7 +18,7 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname.userName);
+// console.log(regularUser.fullname.userName);
 
 //if there any no fullname we can use ? to check 
 // console.log(regularUser.fullname?.userName);
@@ -28,18 +28,18 @@ const obj2 = {3: "a", 4: "b"}
 
 // const obj3 = {obj1 ,obj2};
 const obj3 = Object.assign(obj1, obj2)
-console.log(obj3);
+// console.log(obj3);
 
 //another good approach to combine objects use {} before objects
 
 // if we are using {} the all objects source are targeted to {}, (target, source), if not then all objects are targeted to the first object.
 
 const obj4 = Object.assign({}, obj1, obj2);
-console.log(obj4);
+// console.log(obj4);
 
 //combine object using spread operator, most of the time we are this syntax rather than above syntax
 const obj5 = {...obj1, ...obj2};
-console.log(obj5);
+// console.log(obj5);
 
 const usersInfo = [
     {
@@ -65,13 +65,46 @@ const usersInfo = [
     
 ]
 
-console.log(usersInfo[2].email);
+// console.log(usersInfo[2].email);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));//very important 
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));//very important 
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+/***************Object de-structure and JSON API ******************/
+
+
+const course = {
+    coursename : "Javascript",
+    price: "1200",
+    lecturer: "Gagan Sunar"
+}
+
+//course.coursename
+
+const {coursename} = course;
+console.log(course);
+//or
+const {coursename: instructor} = course;
+console.log(instructor);
+
+
+//react example use of object de structured
+const navbar = ({company}) => {
+
+}
+navbar(company = "gagan");
+
+// JSON
+// {
+//     "name": "gagan",
+//     "coursename": "javascript",
+//     "price": "free"
+// }
+
+
 
